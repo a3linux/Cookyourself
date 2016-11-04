feb31c5f8967b802e097bc9e31745a9376e65207
“stormysun513” <stormysun513@hotmail.com>
2016-11-03 23:33:24 -0400

    [yulunt] modify base.html

--- src/cookyourself/urls.py
+++ src/cookyourself/urls.py
@@ -4,4 +4,8 @@ import cookyourself.views
 
 urlpatterns = [
     url(r'^$', cookyourself.views.index, name='index'),
+    url(r'^dish/(?P<id>\d+)$', cookyourself.views.dish, name='dish'),
+    url(r'^shoppinglist$', cookyourself.views.shoppinglist, name='shoppinglist'),
+    url(r'^recommendation$', cookyourself.views.recommendation, name='recommendation'),
+    url(r'^profile/(?P<id>\d+)$', cookyourself.views.profile, name='profile'),
 ]
--- src/cookyourself/views.py
+++ src/cookyourself/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
-
 
 # Create your views here.
 def index(request):
-    return render_to_response('main.html')
+    return render(request, 'main.html')
+
+def dish(request, id=0):
+    return render(request, 'dish.html')
+
+def shoppinglist(request):
+    return render(request, 'shoppinglist.html')
+
+def recommendation(request):
+    return render(request, 'recommendation.html')
+
+def profile(request, id=0):
+    return render(request, 'profile_i_made_it.html')
