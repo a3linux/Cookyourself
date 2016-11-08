862c220cfd542f8b62bed93b8bc99a4014da758c
“stormysun513” <stormysun513@hotmail.com>
2016-11-08 14:52:55 -0500

    [yulunt] complete responsive youtube iframe in dish.html

--- src/cookyourself/static/cookyourself/js/base.js
+++ src/cookyourself/static/cookyourself/js/base.js
@@ -3,17 +3,5 @@
  */
 
 $(document).ready(function () {
-
     document.getElementById("timestamp").innerHTML = String(new Date().getFullYear());
-
-    // var clockElement = document.getElementById("clock");
-    // function updateClock(clock) {
-    //     clock.innerHTML = moment().format('MMM D, YYYY, h:mm:ss a')
-    //     // MMM D, YYYY, h:mm:ss a
-    // }
-    // updateClock(clockElement);
-    //
-    // setInterval(function () {
-    //     updateClock(clockElement);
-    // }, 60000);
 });
