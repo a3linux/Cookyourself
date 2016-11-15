7a7d6906a3b150c8c30549631b9f326b4d97d00d
“stormysun513” <stormysun513@hotmail.com>
2016-11-14 22:33:09 -0500

    [yulunt] modified typo 'discription' in models.py

--- src/cookyourself/models.py
+++ src/cookyourself/models.py
@@ -21,7 +21,7 @@ class DishImage(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=128)
-    price = models.IntegerField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return self.name
