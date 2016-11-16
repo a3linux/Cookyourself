916e742bd728692382cff5017bff42b4fd0c5855
AlicewD <dwnapply@hotmail.com>
2016-11-15 21:16:17 -0500

    aa

--- src/cookyourself/static/cookyourself/js/base.js
+++ src/cookyourself/static/cookyourself/js/base.js
@@ -89,8 +89,6 @@ function getUserInfo(response) {
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
         $("#fb-signup").show(); 
         $("#fb-login").show();
         $("#fb-logout").hide(); 
