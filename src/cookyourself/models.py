f4f9fb69cb3a47d970563e1bc8c4093d5b588b87
AlicewD <dwnapply@hotmail.com>
2016-11-16 16:15:51 -0500

    aaa

--- src/cookyourself/models.py
+++ src/cookyourself/models.py
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
     photo = models.ImageField(upload_to='img/', blank=True, null=True)
 
     def __str__(self):
-        return str(self.username)
+        return str(self.user)
 
 
 # One-to-many-image-field reference:
